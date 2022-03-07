7a3b1161c8ed7430b4fa84aeaac29097538a01de
kowwwwwb <201980020@psu.palawan.edu.ph>
2022-03-08 03:54:28 +0800

    commit by Kobe

---
+++ legendary-garbanzo/Practice_5-04 answer.md
@@ -0,0 +1,8 @@
+:question:What function do we use to start a session?
+
+The session_start() function is used and should always be first and before HTML tags.
+
+
+:question:Where are these session variables stored? 
+
+The session variables are stored on the server.
--- legendary-garbanzo/web3/calc.php
+++ legendary-garbanzo/web3/calc.php
@@ -2,15 +2,33 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+     $val1 = $_POST['val1'];
+     $val2 = $_POST['val2'];
+     $operator = $_POST['calc'];
+    
+  
         # test if input is_numeric
-        if(1){
+        if(is_numeric($val1) && is_numeric($val2))
+        {
             # test if calc is not null
-            if(1){
-                # switch statement for value of calc
-                switch(1){
-                    
+            if(!empty($operator))
+            { # switch statement for value of calc
+                switch($operator)
+                {
+                    case "add":
+                        $result = $val1+$val2;
+                        break;
+                    case "sub":
+                        $result = $val1-$val2;
+                        break;
+                    case "mul":
+                        $result = $val1*$val2; 
+                        break;
+                    case "div":
+                        $result = $val1/$val2;
+                        break;
                 }
-                echo "Calculation result: $result";
+                echo "<p>Calculation result: $result</p>";
             }
         }
         else{ echo "Invalid entry - please retry"; }
